e0164a0feb2625bdfb31e27a1ee9bfff2a41c539
Doomer13 <77550237+Doomer13@users.noreply.github.com>
2024-11-21 11:16:56 +0300

    Добавить тест, проверяющий, что первая ссылка в выдаче ведет на сайт https://www.selenium.dev/

--- src/test/java/org/example/seleniumproject/MainPageTest.java
+++ src/test/java/org/example/seleniumproject/MainPageTest.java
@@ -1,5 +1,6 @@
 package org.example.seleniumproject;
 
+import org.checkerframework.framework.qual.DefaultQualifier;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.devtools.v123.layertree.model.Layer;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.ArrayList;
@@ -37,16 +41,32 @@ public class MainPageTest {
     public void search() {
 
         String input = "Selenium";
-
         WebElement searchField = driver.findElement(By.cssSelector("#sb_form_q"));
         searchField.sendKeys(input);
         searchField.submit();
+        List<WebElement> elementList =  driver.findElements(By.cssSelector(":not(.b_adurl) > cite"));
 
+        proverka(elementList,0);
+        String url = driver.getCurrentUrl();
+        assertEquals("https://www.selenium.dev/", url);
 
-        List<WebElement> elementList =  driver.findElement(By.cssSelector(":not(.b_adurl)>cite"));
-        
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals("Selenium", searchPageField.getAttribute("value"));
+        //WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
+        //assertEquals("Selenium", searchPageField.getAttribute("value"));
     }
 
+    public void proverka (List<WebElement> elementList, int num){
+
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(6));
+        wait.until(ExpectedConditions.and(
+                ExpectedConditions.textToBePresentInElementLocated(By.cssSelector(":not(.b_adurl) > cite"), "selenium"),
+                ExpectedConditions.elementToBeClickable(By.cssSelector(":not(.b_adurl) > cite"))
+        ));
+
+        ArrayList tabs = new ArrayList<> (driver.getWindowHandles());
+        if (tabs.size() > 1) driver.switchTo().window(tabs.get(1).toString());
+
+        elementList.get(0).click();
+
+    }
 }
